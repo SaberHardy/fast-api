263b8139abeda10e7130f667a5b323c747bc862d
SaberHardy <samirsamado5@gmail.com>
2022-05-31 20:43:06 +0300

    get and add item

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import structure_model
 
 app = FastAPI()
 
@@ -8,6 +9,21 @@ fakeDatabase = {
     3: {'task': 'Make a test'},
 }
 
+
 @app.get('/')
 def get_items():
+    return fakeDatabase
+
+
+# Get items by id
+@app.get('/{id}')
+def get_item(id: int):
+    return fakeDatabase[id]
+
+
+@app.post('/')
+def add_item(item: structure_model.Item):
+    new_id = len(fakeDatabase.keys()) + 1
+    fakeDatabase[new_id] = {'item': item.task}
+
     return fakeDatabase
