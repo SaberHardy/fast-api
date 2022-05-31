a6f3c17a0a6f16caebeff3b4b2dbc14f6a904fb7
SaberHardy <samirsamado5@gmail.com>
2022-05-31 22:36:40 +0300

    crud using Sessions

--- main.py
+++ main.py
@@ -30,26 +30,31 @@ def get_items(session: Session = Depends(get_session)):
 
 # Get items by id
 @app.get('/{id}')
-def get_item(id: int):
-    return fakeDatabase[id]
+def get_item(id: int, session: Session = Depends(get_session)):
+    item = session.query(models.Item).get(id)
+    return item
 
 
 # Option 1 using the item class in structure_model model
-# @app.post('/')
-# def add_item(item: structure_model.Item):
-#     new_id = len(fakeDatabase.keys()) + 1
-#     fakeDatabase[new_id] = {'task': item.task}
-#
-#     return fakeDatabase
+@app.post('/')
+def add_item(item: structure_model.Item, session: Session = Depends(get_session)):
+    # new_id = len(fakeDatabase.keys()) + 1 # the id will auto generated
+    # fakeDatabase[new_id] = {'task': item.task}
+    item = models.Item(task=item.task)
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+
+    return item
 
 
 # Option 2 using body class (this is the best way to create your data)
-@app.post('/')
-def add_item(body=Body()):
-    new_id = len(fakeDatabase.keys()) + 1
-    fakeDatabase[new_id] = {'task': body['task']}
-
-    return fakeDatabase
+# @app.post('/')
+# def add_item(body=Body()):
+#     new_id = len(fakeDatabase.keys()) + 1
+#     fakeDatabase[new_id] = {'task': body['task']}
+#
+#     return fakeDatabase
 
 
 # Option 3 manually create the request
@@ -61,12 +66,18 @@ def add_item(body=Body()):
 #     return fakeDatabase
 
 @app.put('/{id}')
-def update_item(id: int, item: structure_model.Item):
-    fakeDatabase[id]['task'] = item.task
-    return fakeDatabase
+def update_item(id: int, item: structure_model.Item, session: Session = Depends(get_session)):
+    item_object = session.query(models.Item).get(id)
+    item_object.task = item.task
+    session.commit()
+    return item_object
 
 
 @app.delete('/{id}')
-def delete_item(id: int):
-    del fakeDatabase[id]
-    return fakeDatabase
+def delete_item(id: int, session: Session = Depends(get_session)):
+    item_object = session.query(models.Item).get(id)
+    session.delete(item_object)
+    session.commit()
+    session.close()
+    # del fakeDatabase[id]
+    return "Item was deleted..."
