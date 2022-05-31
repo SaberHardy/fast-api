8af1fdd9056f48a49373ca64a1f9816be90aa9ac
SaberHardy <samirsamado5@gmail.com>
2022-05-31 21:53:14 +0300

    update_item & delete_item

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 import structure_model
 
 app = FastAPI()
@@ -21,9 +21,39 @@ def get_item(id: int):
     return fakeDatabase[id]
 
 
-@app.post('/')
-def add_item(item: structure_model.Item):
-    new_id = len(fakeDatabase.keys()) + 1
-    fakeDatabase[new_id] = {'item': item.task}
+# Option 1 using the item class in structure_model model
+# @app.post('/')
+# def add_item(item: structure_model.Item):
+#     new_id = len(fakeDatabase.keys()) + 1
+#     fakeDatabase[new_id] = {'task': item.task}
+#
+#     return fakeDatabase
 
+
+# Option 2 using body class (this is the best way to create your data)
+@app.post('/')
+def add_item(body=Body()):
+    new_id = len(fakeDatabase.keys()) + 1
+    fakeDatabase[new_id] = {'task': body['task']}
+
+    return fakeDatabase
+
+
+# Option 3 manually create the request
+# @app.post('/')
+# def add_item(body=Body()):
+#     new_id = len(fakeDatabase.keys()) + 1
+#     fakeDatabase[new_id] = {'task': body['task']}
+#
+#     return fakeDatabase
+
+@app.put('/{id}')
+def update_item(id: int, item: structure_model.Item):
+    fakeDatabase[id]['task'] = item.task
+    return fakeDatabase
+
+
+@app.delete('/{id}')
+def delete_item(id: int):
+    del fakeDatabase[id]
     return fakeDatabase
